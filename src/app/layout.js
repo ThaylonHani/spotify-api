b0a4580a51a2964c8e0f2a7e447e37caf9ff2845
Thaylon Haniel <thaylonhaniel9@gmail.com>
2023-06-29 16:10:07 -0300

    add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
-import { Header } from '@/components/header'
 import './globals.css'
 
 
 export const metadata = {
   title: 'SyncSpot',
   description: 'Veja suas músicas',
+  
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet"/>
+      </head>
       <body >
         {children}
       </body>
